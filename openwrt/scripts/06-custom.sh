8db8b908e8a0996b906ec6bba9eecedd95be8546
pmkol <pmkol@users.noreply.github.com>
2025-07-21 01:39:01 +0800

    package: rename luci-app-openlist2

--- openwrt/scripts/06-custom.sh
+++ openwrt/scripts/06-custom.sh
@@ -2,10 +2,10 @@
 
 ### Add new packages or patches below
 ### For example, download openlist from a third-party repository to package/new/openlist
-### Then, add CONFIG_PACKAGE_luci-app-openlist=y to the end of openwrt/23-config-common-custom
+### Then, add CONFIG_PACKAGE_luci-app-openlist2=y to the end of openwrt/23-config-common-custom
 
 # openlist - add new package
-git clone https://$github/sbwml/luci-app-openlist package/new/openlist
+git clone https://$github/sbwml/luci-app-openlist2 package/new/openlist
 
 # lrzsz - add patched package
 rm -rf feeds/packages/utils/lrzsz
