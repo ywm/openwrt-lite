90ed07d658e899ae0a4afea994e08dcaa8f21927
pmkol <pmkol@users.noreply.github.com>
2025-09-01 09:01:07 +0800

    golang: update to 1.25.x

--- openwrt/scripts/02-prepare_package.sh
+++ openwrt/scripts/02-prepare_package.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -e
 
-# golang - 1.24
+# golang - 1.25
 rm -rf feeds/packages/lang/golang
-pkg_golang=$(curl -s https://github.com/pmkol/openwrt-gh-action-sdk/commit/a67931ef2b14e7c4cf632f277a2263d9c61f980f.patch | awk '/^\+.*packages_lang_golang/ {sub(/^\+/, ""); print}' | sed -n 's/.*git clone https:\/\/github.com\/\(.*\) feeds\/packages\/lang\/golang/\1/p')
+pkg_golang=$(curl -s https://github.com/pmkol/openwrt-gh-action-sdk/commit/5a59e9de7ceac6be1df33e8182897781f38336e7.patch | awk '/^\+.*packages_lang_golang/ {sub(/^\+/, ""); print}' | sed -n 's/.*git clone https:\/\/github.com\/\(.*\) feeds\/packages\/lang\/golang/\1/p')
 git clone https://$github/$pkg_golang --depth 1 feeds/packages/lang/golang
 [ "$DEV_BUILD" = "y" ] && sed -i 's/GO_AMD64:=v1/GO_AMD64:=v2/g' feeds/packages/lang/golang/golang-values.mk
 
